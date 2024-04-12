367ab5766cab5c072ef88730eb88743e46dd820c
JayKrishnaMandal <80018947+JayKrishnaMandal@users.noreply.github.com>
2024-04-12 08:59:48 +0545

    corrected mistakes

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,22 @@
 import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Blogs from "./pages/Blogs";
+import Contact from "./pages/Contact";
+import NoPage from "./pages/NoPage";
 
 function App() {
   return (
     <>
-      <h1>hello world</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />}>
+            <Route path="blogs" element={<Blogs />} />
+            <Route path="contact" element={<Contact />} />
+            <Route path="*" element={<NoPage />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
